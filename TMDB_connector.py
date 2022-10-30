4b6f1bc8f257e9aa1fcc69ddf747c13fa7b439d9
khalilbenkhaled <80462097+khalilbenkhaled@users.noreply.github.com>
2022-10-30 20:27:58 +0100

    Tmdb integration (#2)
    * add a function which returns the tvshow id
    
    * add the api key
    
    * add the function that feches the date of the last episode

--- TMDB_connector.py
+++ TMDB_connector.py
@@ -11,3 +11,20 @@ def get_id(name):
     if json['results']:
         id = json['results'][0]['id']
         return id
+
+
+
+def fetcher(name):
+    id = get_id(name)
+    if id:
+        url = f'https://api.themoviedb.org/3/tv/{id}?api_key={API_KEY}'
+        response = requests.get(url)
+        json = response.json()
+        episode = json['next_episode_to_air']
+        result = {
+                'date': episode['air_date'],
+                'episode_number': episode['episode_number'],
+                'name': episode['name']
+                }
+        return result
+
